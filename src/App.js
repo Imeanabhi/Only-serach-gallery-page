da5585ce11c76fa178a9827a638485855408ba8c
Imeanabhi <abhi.nagabothu@gmail.com>
2024-06-14 15:42:55 +0530

    added all changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import axios from 'axios';
+import React, { useState } from 'react';
+import Gallery from './Gallery';
+const apiKey = "636e1481b4f3c446d26b8eb6ebfe7127";
+const App = () => {
+const[search , setSearch]=useState("");
+const[data,setData]=useState([]);
+const changeHandler=e=>{
+setSearch(e.target.value);
 }
+const submitHandler=e=>{
+e.preventDefault();
+axios.get(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${search}&per_page=24&format=json&nojsoncallback=1`).then(
+  response=>setData(response.data.photos.photo))
+console.log(search);
+}
+  return (
+    <div>
+      <center>
+        <form onSubmit={submitHandler}>
+          <h2>
+            Gallery snapshots
+          </h2>
+          <input type="text" size="30px" value ={search} onChange={changeHandler} /> 
+          <br /><br/>
+          <input type="submit" name="search"/>
+        </form>
+        <br />
+        {data.length>=1? <Gallery data={data}/>:<h4>No data loaded</h4>}
+      </center>
 
-export default App;
+    </div>
+  )
+}
+export default App
